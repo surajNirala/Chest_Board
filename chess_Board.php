8bdc2cbc1a00fd341235780037a1a0611dc1c712
suraj <suraj.nirala1995@gmail.com>
2019-06-21 18:22:04 +0530

    Create chess_Board.php

---
+++ chess_Board.php
@@ -0,0 +1,22 @@
+<?php 
+echo "<table border='5px'>";
+for($row=1;$row<=8;$row++)
+{
+          echo "<tr>";
+          for($col=1;$col<=8;$col++)
+{
+          $total=$row+$col;
+          if($total%2==0)
+  {
+          echo "<td height=30px width=30px bgcolor=#FFFFFF></td>";
+          }
+ else
+ {
+          echo "<td height=30px width=30px bgcolor=#000000></td>";
+          }
+          }
+          echo "</tr>";
+    }
+         
+echo "<table>";
+ ?>
