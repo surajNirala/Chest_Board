b6f59dfad6f2a8e8791ab9a16c6aea5da1df026a
suraj <suraj.nirala1995@gmail.com>
2019-06-27 18:16:28 +0530

    Update sampark.php

--- sampark.php
+++ sampark.php
@@ -10,5 +10,23 @@ for ($i=$b-1; $i >= 0 ; $i--) {
 }
 
 
+// calculate int number like 12345 = 15 
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "</br>";
+/*$a = 12345;
+
+ $b = strlen($a);
+
+for ($i=$b-1; $i >= 0 ; $i--) { 
+
+	$a = ($a * 10)+$i;
+
+	$a = (int)($a/10);
+
+	echo $a;
+}*/
+
 
 ?>
