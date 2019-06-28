f9d33fcfa3b4b1f8b83c4825557642f7f012b8ea
suraj <suraj.nirala1995@gmail.com>
2019-06-28 16:30:05 +0530

    Create sort.php

---
+++ sort.php
@@ -0,0 +1,11 @@
+<?php 
+
+$inputArray = array(8, 2, 7, 4, 5);
+    $outArray = array();
+    for($x=1; $x<=100; $x++) {
+        if (in_array($x, $inputArray)) {
+            array_push($outArray, $x);
+        }
+    }
+print_r($outArray);
+?>
