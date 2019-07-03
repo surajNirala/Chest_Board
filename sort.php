0ec6b90a5c717b90aaf99fc361699164e361d98f
HeartCoder <34908308+surajNirala@users.noreply.github.com>
2019-07-03 18:32:12 +0530

    Update sort.php

--- sort.php
+++ sort.php
@@ -9,7 +9,29 @@ $inputArray = array(8, 2, 7, 4, 5);
     }
 print_r($outArray);
 
+echo "</br>";
+echo "</br>";
 
+
+echo "Unsorted array is: ";
+echo "<br />";
+print_r($array);
+
+
+for($j = 0; $j < count($array); $j ++) {
+    for($i = 0; $i < count($array)-1; $i ++){
+
+        if($array[$i] > $array[$i+1]) {
+            $temp = $array[$i+1];
+            $array[$i+1]=$array[$i];
+            $array[$i]=$temp;
+        }       
+    }
+}
+
+echo "Sorted Array is: ";
+echo "<br />";
+print_r($array);
 
 //SELECT address, count(id) as cnt FROM list
 //GROUP BY address HAVING cnt > 1 
